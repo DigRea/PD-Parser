d8889c9bd7c8931796a6d7ed443a55fe906abe77
DigRea <146025236+DigRea@users.noreply.github.com>
2025-12-09 12:36:12 +0300

    changed request.py

--- request.py
+++ request.py
@@ -3,14 +3,17 @@ import pprint
 
 url = "https://api.hh.ru/vacancies"
 
-# params = {
-#     'text': 'NAME:(Python OR Java) AND COMPANY_NAME:(1 OR 2 OR YANDEX) AND (DJANGO OR SPRING)',
-#     # страница
-#     'page': 1
-# }
-#
-# result = requests.get(url, params=params).json()
-#
-# pprint.pprint(result)
+name = input("Введите название вакансии: ")
+city = input("Введите город: ")
+
+params = {
+    'text': 'NAME:name',
+    # страница
+    'page': 4
+}
+
+result = requests.get(url, params=params).json()
+
+pprint.pprint(result)
 # print(result['items'][0]['url'])
 # print(result['items'][0]['alternate_url'])
